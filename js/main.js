63776b16d288c9b16cafe78982b222389bb9e351
MichelepioBombai <michelebombai@icloud.com>
2023-01-25 17:06:41 +0100

    fix if/else if

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ const m3_5 = ("fizzbuzz");
 
 
 
-for (let i = 1; i < 100; i++) {
+for (let i = 1; i <= 100; i++) {
 
 
     const listItem = document.createElement("li")
@@ -14,23 +14,23 @@ for (let i = 1; i < 100; i++) {
     listItem.innerHTML = i;
     listItem.classList.add("box");
 
-    if (i % 3 == 0){
-        listItem.classList.add("box3")
-        listItem.innerHTML = m3;
-        
-    }  if(i % 15 == 0){
+    if(i % 15 == 0){
         listItem.classList.add("box3_5")
         listItem.innerHTML = m3_5; 
 
     } 
-      else if(i % 5 == 0){
+    else if (i % 3 == 0){
+        listItem.classList.add("box3")
+        listItem.innerHTML = m3;
+        
+    }
+    else if(i % 5 == 0){
         listItem.classList.add("box5")
         listItem.innerHTML = m5; 
-    
     }
     listEl.append(listItem);
     
-    console.log(listItem);
+    
     
     
 }
